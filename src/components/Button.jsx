4916f7f31c1e8724e0d3e7d5b546e368d5e19bb6
Cassio Santana <cassioo.roger@gmail.com>
2025-07-05 18:45:05 -0300

    feat: add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+import './Button.css';
+
+export default props => {
+  return <button className='button'>{props.label}</button>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Calculator from './Calculator'
+import Calculator from './main/Calculator'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
