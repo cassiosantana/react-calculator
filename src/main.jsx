40b4d17531c68adc8d985b2335baf3e08610e113
Cassio Santana <cassioo.roger@gmail.com>
2025-07-05 12:04:18 -0300

    feat: add Calculator component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import './index.css'
+import Calculator from './Calculator'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <h1>Calculadora</h1>
+      <Calculator />
   </StrictMode>,
 )
